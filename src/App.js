70c6755e0544d8e8174f6dcd72c6b47163b4bea6
Maksym Perlovskyi <103576383+Maksym150678@users.noreply.github.com>
2022-08-23 14:32:00 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Phonebook from './components/Phonebook/Phonebook';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Phonebook />
+    </div>
+  );
+};
+
+export default App;
+
+
+
